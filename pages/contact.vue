e61d2c6fa29c74e87c7b6c31115607791c9e9a2b
amahdy@turndigital.net <aya.mahdy512000@gmail.com>
2024-03-29 22:55:58 +0200

    feat: my cv

--- pages/contact.vue
+++ pages/contact.vue
@@ -25,24 +25,48 @@
     </p>
     <button
       class="generic-btn"
-    
+      @click="downloadFile('Aya Mahdy Senior Frontend 11 - 2023.pdf')"
     >
       Download My CV
     </button>
+
+    <!-- <a :href="pdfUrl" target="_blank">Download PDF</a>
+    <br />
+    <a href="static/AyaMahdy.pdf" target="_blank">Download</a>
+    <br /> -->
   </div>
 </template>
 <script>
 import tabs from "~/components/tabs.vue";
+// import file from "/public/AyaMahdy.pdf";
 export default {
   components: {
     tabs,
   },
   computed: {
-    
+    pdfUrl() {
+     
+      return "/AyaMahdy.pdf"; // Adjust the path if your PDF file is located in a different directory
+    },
   },
   setup() {
-   
+    // console.log(file)
+    const downloadFile = (fileName) => {
+      fetch("static/AyaMahdy.pdf")
+        .then((resp) => resp.blob())
+        .then((blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.style.display = "none";
+          a.href = url;
+          a.download = fileName;
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+        });
+    };
     return {
+      downloadFile,
     };
   },
 };
