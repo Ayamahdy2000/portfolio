d2f6ef5470fec08be3ac18429780c49da738cb76
amahdy@turndigital.net <aya.mahdy512000@gmail.com>
2024-03-30 00:04:04 +0200

    feat: add seo

--- app.vue
+++ app.vue
@@ -3,3 +3,40 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script>
+export default defineNuxtComponent({
+  head() {
+    return {
+      htmlAttrs: {
+        lang: "en",
+      },
+      title: "Aya mahdy",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "'Welcome to Aya Mahdy's website.I am senior frontend developer. Find my latest skills, and contact information.",
+        },
+        {
+          name: "robots",
+          content: "index,follow",
+        },
+
+        // og
+        {
+          property: "og:title",
+          content: "Aya Mahdy",
+        },
+        {
+          property: "og:description",
+          content:
+            "'Welcome to Aya Mahdy's website.I am senior frontend developer. Find my latest skills, and contact information.",
+        },
+      ],
+    };
+  },
+});
+</script>
